978d740b3b6fdae67a837084cd6f00763370929f
skrijwan <skrijwan329@gmail.com>
2025-03-29 01:53:56 +0530

    add images and layout to Giftandmemory component

--- client/src/components/Giftandmemory.jsx
+++ client/src/components/Giftandmemory.jsx
@@ -1,11 +1,73 @@
 import React from 'react'
 import Navbar from './Navbar'
-
+import img1 from "../assets/memoryandgift/pic1.jpg"
+import img2 from "../assets/memoryandgift/pic2.jpg"
+import img3 from "../assets/memoryandgift/pic3.jpg"
+import img4 from "../assets/memoryandgift/pic4.jpg"
+import img5 from "../assets/memoryandgift/pic5.jpg"
+import img6 from "../assets/memoryandgift/pic6.jpg"
+import img7 from "../assets/memoryandgift/pic7.jpg"
+import img8 from "../assets/memoryandgift/pic8.jpg"
+import img9 from "../assets/memoryandgift/pic9.jpg"
+import img10 from "../assets/memoryandgift/pic10.jpg"
+import img11 from "../assets/memoryandgift/pic11.jpg"
+import img12 from "../assets/memoryandgift/pic12.jpg"
+import img13 from "../assets/memoryandgift/pic13.jpg"
+import img14 from "../assets/memoryandgift/pic14.jpg"
+import img16 from "../assets/memoryandgift/pic16.jpg"
+import img17 from "../assets/memoryandgift/pic17.jpg"
+import img18 from "../assets/memoryandgift/pic18.jpg"
+import img19 from "../assets/memoryandgift/pic19.jpg"
+import img20 from "../assets/memoryandgift/pic20.jpg"
+import img21 from "../assets/memoryandgift/pic21.jpg"
+import img22 from "../assets/memoryandgift/pic22.jpg"
+import img23 from "../assets/memoryandgift/pic23.jpg"
 export default function Giftandmemory() {
-  return (
-    <div>
-        <Navbar/>
-      <h1>This is a page</h1>
-    </div>
-  )
+    return (
+        <div>
+            <Navbar />
+            <div className="mianbox" style={{ margin: "20px 0px" }}>
+                <div className="row1" style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center", gap: '20px' }}>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}> <img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img1} alt="" /> </div>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img2} alt="" /></div>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img3} alt="" /></div>
+                </div>
+                <div className="row1" style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center", gap: '20px', marginTop: "20px" }}>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}> <img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img4} alt="" /> </div>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img5} alt="" /></div>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img6} alt="" /></div>
+                </div>
+                <div className="row1" style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center", gap: '20px', marginTop: "20px" }}>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}> <img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img7} alt="" /> </div>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img8} alt="" /></div>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img9} alt="" /></div>
+                </div>
+                <div className="row1" style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center", gap: '20px',    marginTop: "20px" }}>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}> <img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img10} alt="" /> </div>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img11} alt="" /></div>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img12} alt="" /></div>
+                </div>
+                <div className="row1" style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center", gap: '20px',    marginTop: "20px" }}>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}> <img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img13} alt="" /> </div>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img14} alt="" /></div>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img22} alt="" /></div>
+                </div>
+                <div className="row1" style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center", gap: '20px',    marginTop: "20px" }}>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}> <img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img16} alt="" /> </div>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img17} alt="" /></div>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img18} alt="" /></div>
+                </div>
+                <div className="row1" style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center", gap: '20px',    marginTop: "20px" }}>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}> <img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img19} alt="" /> </div>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img20} alt="" /></div>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img21} alt="" /></div>
+                </div>
+                <div className="row1" style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center", gap: '20px',    marginTop: "20px" }}>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}> <img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img23} alt="" /> </div>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img11} alt="" /></div>
+                    <div className="pic" style={{ width: "330px", height: "500px", border: "2px solid white", backdropFilter: "blur(1px)", boxShadow: "#ffffff7d 1px 2px 12px 0px" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img12} alt="" /></div>
+                </div>
+            </div>
+        </div>
+    )
 }
